ffa1f35b51eba14cb41ef616664a5cfe43666d50
Chloe Murphy <csgmurphy@gmail.com>
2024-04-24 15:14:54 +0100

    add variant id

--- dags/sql/reports/completed_order_item_summary.sql
+++ dags/sql/reports/completed_order_item_summary.sql
@@ -12,17 +12,16 @@ SELECT
 	o.idx,
 	o.customer_id AS customer_id,
 	oi.createdat AS item_created,
-	o.ordercreatedat AS order_created,
+	o.createdat AS order_created,
 	oi.updatedat,
 	o.appointment__date AS appointment_date,
 	o.brand_name,
-	o.original_order_name,
-	o.order_name,
-	oi.order_name AS order_name_item,
-	oi.id AS unique_item_id,
+	o.order_type AS order__order_type,
+	oi.order_type AS item__order_type,
 	oi.order_id,
-	o.order_type AS order_type_initial,
-	oi.order_type AS order_type,
+	oi.original_order_name AS partner_order_name,
+	oi.order_name AS harper_order_name,
+	oi.id AS item_id,
 	oi.original_name AS product_name, -- need to clean sizes for certain brands in raw
 	CASE
         WHEN POSITION('-' IN oi.original_name) > 0 THEN
@@ -30,6 +29,7 @@ SELECT
         ELSE
             oi.original_name
     END AS product_name_cleaned,
+	oi.variant_id,
 	oi.sku,
 	oi.price as item_price_pence,
 	oi.discount AS discount_price_pence,
@@ -41,10 +41,10 @@ SELECT
     CASE WHEN (oi.purchased != TRUE AND oi.returned != TRUE AND o.order_status = 'completed') THEN 1 ELSE 0 END AS unpurchased,
 	CASE WHEN oi.return_sent_by_customer = TRUE THEN 1 ELSE 0 END AS return_sent,
 	CASE WHEN oi.received_by_warehouse = TRUE THEN 1 ELSE 0 END AS return_received,
-	CASE WHEN oi.is_initiated_sale = TRUE THEN 1 ELSE 0 END AS is_initiated_sale,
 	CASE WHEN oi.out_of_stock = TRUE THEN 1 ELSE 0 END AS out_of_stock,
 	CASE WHEN oi.preorder = TRUE THEN 1 ELSE 0 END AS preorder,
 	CASE WHEN oi.received = TRUE THEN 1 ELSE 0 END AS received,
+	CASE WHEN oi.is_initiated_sale = TRUE THEN 1 ELSE 0 END AS is_initiated_sale,
 	CASE WHEN oi.initiated_sale__user_role like '%%remote_sales%%' THEN 1 ELSE 0 END AS inspire_me_flag,
 	{{ dim__time_columns | prefix_columns('cdt', 'createdat')}}
 FROM orders o
@@ -56,7 +56,7 @@ WHERE
 	AND oi.name IS NOT NULL AND oi.name != ''
 	AND oi.order_name IS NOT NULL AND oi.order_name != ''
 	AND o.brand_name != 'Harper Production'
-	AND o.order_status IN ['completed','returned','unpurchased_processed']
+	AND o.order_status IN ('completed','returned','unpurchased_processed')
 	AND o.link_order_child = FALSE
 	AND oi.createdat >= '2022-01-01';
 
