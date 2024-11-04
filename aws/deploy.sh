96e9def2e24058c1fc34fc7743d50188818ef43d
Martin Moss <martyzz1@gmail.com>
2024-11-04 12:11:08 +0000

    [main] Automate getting IP address ranges for Tableau from salesforce

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -13,6 +13,16 @@ BastionSecurityGroupId=$(aws cloudformation describe-stacks --stack-name product
 DatalakeAdminUser=$(op item get "$STAGE-datalake" --format json --vault Environments --fields DATALAKE_ADMINUSER | jq -r '.value' || exit 1)
 DatalakeAdminPass=$(op item get "$STAGE-datalake" --format json --vault Environments --fields DATALAKE_ADMINPASS | jq -r '.value' || exit 1)
 
+# Fetch the IP ranges for Tableau DUB01 (eu-central-1 region)
+# Fetch the IP ranges for Tableau DUB01 (eu-central-1 region)
+TableauIPs=()
+while IFS= read -r ip; do
+    TableauIPs+=("$ip")
+done < <(curl -s https://ip-ranges.salesforce.com/ip-ranges.json | jq -r '.prefixes[] | select(.region=="eu-central-1") | .ip_prefix[]')
+
+TableauIP1="${TableauIPs[0]}"
+TableauIP2="${TableauIPs[1]:-}"
+
 # in case of previous failures run this
 # aws cloudformation delete-stack --stack-name "$stage-harper-vpc-stack"
 
@@ -25,5 +35,7 @@ aws cloudformation $CMD \
       ParameterKey=DatalakeAdminUser,ParameterValue="${DatalakeAdminUser}" \
       ParameterKey=DatalakeAdminPass,ParameterValue="${DatalakeAdminPass}" \
       ParameterKey=BastionSecurityGroupId,ParameterValue="${BastionSecurityGroupId}" \
+      ParameterKey=TableauIP1,ParameterValue="${TableauIP1}" \
+      ${TableauIP2:+ParameterKey=TableauIP2,ParameterValue="${TableauIP2}"} \
     --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
     --region eu-west-1 \
