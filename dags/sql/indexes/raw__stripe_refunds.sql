b8e20ee14d049a1dc41e875b86de546387760362
Martin Moss <martyzz1@gmail.com>
2024-11-14 16:59:57 +0000

    [ASNA-12324/stripeReport] Bugfixes

--- dags/sql/indexes/raw__stripe_refunds.sql
+++ dags/sql/indexes/raw__stripe_refunds.sql
@@ -1,3 +1,2 @@
-CREATE INDEX IF NOT EXISTS raw__stripe__refunds__id_idx ON {{ schema }}.raw__stripe__refunds (id);
 CREATE INDEX IF NOT EXISTS raw__stripe__refunds__charge_idx ON {{ schema }}.raw__stripe__refunds (charge);
 CREATE INDEX IF NOT EXISTS raw__stripe__refunds__created_idx ON {{ schema }}.raw__stripe__refunds (created);
