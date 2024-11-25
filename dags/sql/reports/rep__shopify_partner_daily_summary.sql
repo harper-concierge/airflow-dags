e6be402e483e36b188ef0b7c34e6cf05e0993a7d
Chloe Murphy <csgmurphy@gmail.com>
2024-11-25 17:31:30 +0000

    cleaning up & joining daily and monthly

--- dags/sql/reports/rep__shopify_partner_daily_summary.sql
+++ dags/sql/reports/rep__shopify_partner_daily_summary.sql
@@ -3,79 +3,96 @@ DROP MATERIALIZED VIEW IF EXISTS {{ schema }}.rep__shopify_partner_daily_summary
 {% endif %}
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS {{ schema }}.rep__shopify_partner_daily_summary AS
-    WITH orders AS (
-        SELECT
-            CASE
-                WHEN (order_type = 'harper_try' OR harper_product = 'harper_try') THEN 'harper_try'
-                WHEN (tags LIKE '%%harper%%' OR payment_gateway_names LIKE '%%Harper Payments%%' OR co.original_order_name IS NOT NULL) THEN 'harper_concierge'
-                ELSE NULL
-            END AS harper__product,
-            po.name,
-            po.created_at::date as order_date,
-            po.partner__name,
-            po.partner__reference,
-            po.items_ordered,
-            po.items_returned,
-            po.value_ordered,
-            po.value_returned,
-            po.cancelled_at,
-            po.shipping_address__city,
-            CASE
-                WHEN tags LIKE '%%harper%%' OR payment_gateway_names LIKE '%%Harper Payments%%' OR co.order_name IS NOT NULL THEN 'Harper'
-                WHEN source_name = 'web' THEN 'Web'
-            END AS source,
-            CASE WHEN ((items_ordered::bigint) - (items_returned::bigint)) >= 1 THEN 1 ELSE 0 END AS keep,
-            CASE
-                WHEN LOWER(po.shipping_address__city) LIKE '%%london%%' THEN 'London'
-                ELSE 'Not London'
-            END AS london
-        FROM {{ schema }}.shopify_partner_orders po
-        LEFT JOIN {{ schema }}.clean__order__summary co
-        ON po.name = co.original_order_name
-    )
+    WITH base_orders AS (
+   SELECT
+       CASE
+           WHEN (order_type = 'harper_try' OR harper_product = 'harper_try') THEN 'harper_try'
+           WHEN (tags LIKE '%%harper%%' OR payment_gateway_names = 'Harper Payments' OR co.original_order_name IS NOT NULL) THEN 'harper_concierge'
+           ELSE NULL
+       END AS harper__product,
+       po.name,
+       CASE
+           WHEN LOWER(po.shipping_address__city) = 'london' THEN 'London'
+           ELSE 'Outside_London'
+       END AS region,
+       po.created_at,
+       po.partner__name,
+       po.partner__reference,
+       po.items_ordered,
+       po.items_returned,
+       po.value_ordered,
+       po.value_returned,
+       po.cancelled_at,
+       CASE
+           WHEN tags LIKE '%%harper%%' OR payment_gateway_names = 'Harper Payments' OR co.order_name IS NOT NULL THEN 'Harper'
+           WHEN source_name = 'web' THEN 'Web'
+       END AS source,
+       CASE WHEN ((items_ordered::bigint) - (items_returned::bigint)) >= 1 THEN 1 ELSE 0 END AS keep
+   FROM {{ schema }}.shopify_partner_orders po
+   LEFT JOIN {{ schema }}.clean__order__summary co ON po.name = co.order_name
+),
+
+london_data AS (
+   SELECT *,
+   CASE
+       WHEN region = 'London' THEN 'London'
+       ELSE NULL
+   END AS analysis_region
+   FROM base_orders
+   UNION ALL
+   SELECT *,
+   'Regional' as analysis_region
+   FROM base_orders
+)
 
 SELECT
-    ROW_NUMBER() OVER (ORDER BY order_date, partner__name, london) as id,
-    order_date,
-    partner__name,
-    partner__reference,
-    london,
-    source,
-    harper__product,
-    COUNT(DISTINCT name) AS orders,
-    COALESCE(SUM(items_ordered), 0) AS total_items_ordered,
-    COALESCE(SUM(items_returned), 0) AS total_items_returned,
-    COALESCE(SUM(items_ordered) - SUM(items_returned), 0) AS total_items_kept,
-    COALESCE(ROUND(SUM(value_ordered)::decimal, 2), 0) AS total_value_ordered,
-    COALESCE(ROUND(SUM(value_returned)::decimal, 2), 0) AS total_value_returned,
-    COALESCE(ROUND((SUM(value_ordered) - SUM(value_returned))::decimal, 2), 0) AS total_value_kept,
-    COALESCE(ROUND((SUM(value_ordered)/NULLIF(COUNT(DISTINCT name), 0))::decimal, 2), 0) AS gross_AOV,
-    COALESCE(ROUND(((SUM(value_ordered) - SUM(value_returned))/NULLIF(COUNT(DISTINCT CASE WHEN cancelled_at IS NULL THEN name ELSE NULL END), 0))::decimal, 2), 0) AS net_ATV,
-    COALESCE(ROUND((SUM(items_ordered)/NULLIF(COUNT(DISTINCT name), 0))::decimal, 2), 0) AS gross_UPT,
-    COALESCE(ROUND(((SUM(items_ordered) - SUM(items_returned))/NULLIF(COUNT(DISTINCT CASE WHEN cancelled_at IS NULL THEN name ELSE NULL END), 0))::decimal, 2), 0) AS net_UPT,
-    COALESCE(ROUND((SUM(value_ordered)/NULLIF(SUM(items_ordered), 0))::decimal, 2), 0) AS gross_ASP,
-    COALESCE(ROUND(((SUM(value_ordered) - SUM(value_returned))/NULLIF((SUM(items_ordered) - SUM(items_returned)), 0))::decimal, 2), 0) AS net_ASP,
-    COALESCE(ROUND(((SUM(items_ordered) - SUM(items_returned))/NULLIF(SUM(items_ordered), 0))::decimal, 2), 0) AS purchase_rate_items,
-    COALESCE(ROUND(((SUM(value_ordered) - SUM(value_returned))/NULLIF(SUM(value_ordered), 0))::decimal, 2), 0) AS purchase_rate_value,
-    COALESCE(ROUND((SUM(items_returned)/NULLIF(SUM(items_ordered), 0))::decimal, 2), 0) AS return_rate_items,
-    COALESCE(ROUND((SUM(value_returned)/NULLIF(SUM(value_ordered), 0))::decimal, 2), 0) AS return_rate_value,
-    ROUND((SUM(keep)::numeric/NULLIF(COUNT(DISTINCT name), 0)), 2) AS keep_rate
-FROM orders
+   ROW_NUMBER() OVER (ORDER BY TO_CHAR(created_at, 'YYYY-MM-dd'), partner__name) as id,
+   TO_CHAR(created_at, 'YYYY-MM-dd') AS day,
+   TO_CHAR(created_at, 'YYYY-MM-01') AS year_month,
+   partner__name,
+   partner__reference,
+   source,
+   harper__product,
+   analysis_region,
+   COUNT(DISTINCT name) AS orders,
+   COALESCE(SUM(items_ordered), 0) AS total_items_ordered,
+   COALESCE(SUM(items_returned), 0) AS total_items_returned,
+   COALESCE(SUM(items_ordered) - SUM(items_returned), 0) AS total_items_kept,
+   COALESCE((ROUND(SUM(value_ordered)::decimal, 2))::decimal, 0) AS total_value_ordered,
+   COALESCE((ROUND(SUM(value_returned)::decimal, 2))::decimal, 0) AS total_value_returned,
+   COALESCE((ROUND((SUM(value_ordered) - SUM(value_returned))::decimal, 2))::decimal, 0) AS total_value_kept,
+   COALESCE((ROUND((SUM(value_ordered)/NULLIF(COUNT(DISTINCT name), 0))::decimal, 2))::decimal, 0) AS gross_AOV,
+   COALESCE((ROUND(((SUM(value_ordered) - SUM(value_returned))/NULLIF(COUNT(DISTINCT CASE WHEN cancelled_at IS NULL THEN name ELSE NULL END), 0))::decimal, 2))::decimal, 0) AS net_ATV,
+   COALESCE((ROUND((SUM(items_ordered)/NULLIF(COUNT(DISTINCT name), 0))::decimal, 2))::decimal, 0) AS gross_UPT,
+   COALESCE((ROUND(((SUM(items_ordered) - SUM(items_returned))/NULLIF(COUNT(DISTINCT CASE WHEN cancelled_at IS NULL THEN name ELSE NULL END), 0))::decimal, 2))::decimal, 0) AS net_UPT,
+   COALESCE((ROUND((SUM(value_ordered)/NULLIF(SUM(items_ordered), 0))::decimal, 2))::decimal, 0) AS gross_ASP,
+   COALESCE((ROUND(((SUM(value_ordered) - SUM(value_returned))/NULLIF((SUM(items_ordered) - SUM(items_returned)), 0))::decimal, 2))::decimal, 0) AS net_ASP,
+   COALESCE((ROUND(((SUM(items_ordered) - SUM(items_returned))/NULLIF(SUM(items_ordered), 0))::decimal, 2))::decimal, 0) AS purchase_rate_items,
+   COALESCE((ROUND(((SUM(value_ordered) - SUM(value_returned))/NULLIF(SUM(value_ordered), 0))::decimal, 2))::decimal, 0) AS purchase_rate_value,
+   COALESCE((ROUND((SUM(items_returned)/NULLIF(SUM(items_ordered), 0))::decimal, 2))::decimal, 0) AS return_rate_items,
+   COALESCE((ROUND((SUM(value_returned)/NULLIF(SUM(value_ordered), 0))::decimal, 2))::decimal, 0) AS return_rate_value,
+   ROUND((SUM(keep)::numeric/NULLIF(COUNT(DISTINCT name), 0)),2) AS keep_rate
+FROM london_data
 WHERE source IN ('Web','Harper')
+AND analysis_region IS NOT NULL
 GROUP BY
-    order_date,
-    partner__name,
-    partner__reference,
-    london,
-    source,
-    harper__product;
+   partner__name,
+   partner__reference,
+   TO_CHAR(created_at, 'YYYY-MM-dd'),
+   TO_CHAR(created_at, 'YYYY-MM-01'),
+   source,
+   harper__product,
+   analysis_region;
 
 {% if is_modified %}
 CREATE UNIQUE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_id_idx ON {{ schema }}.rep__shopify_partner_daily_summary(id);
-CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_date_idx ON {{ schema }}.rep__shopify_partner_daily_summary(order_date);
+CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_day_idx ON {{ schema }}.rep__shopify_partner_daily_summary(day);
+CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_year_month_idx ON {{ schema }}.rep__shopify_partner_daily_summary(year_month);
 CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_partner_idx ON {{ schema }}.rep__shopify_partner_daily_summary(partner__name);
-CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_london_idx ON {{ schema }}.rep__shopify_partner_daily_summary(london);
-CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_source_idx ON {{ schema }}.rep__shopify_partner_daily_summary(source);
+CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_partner__reference_idx ON {{ schema }}.rep__shopify_partner_daily_summary(partner__reference);
+CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_analysis_region_idx ON {{ schema }}.rep__shopify_partner_daily_summary(analysis_region);
+CREATE INDEX IF NOT EXISTS rep__shopify_partner_daily_summary_harper__product_idx ON {{ schema }}.rep__shopify_partner_daily_summary(harper__product);
+
 {% endif %}
 
 REFRESH MATERIALIZED VIEW {{ schema }}.rep__shopify_partner_daily_summary;
