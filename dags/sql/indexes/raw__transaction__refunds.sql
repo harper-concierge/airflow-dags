48b6eec1984e92323f341b438be84cf6b4b62aa3
Martin Moss <martyzz1@gmail.com>
2024-12-05 00:48:16 +0000

    [ASNA-12345/indexing_and_optimisations] Storing the work so far

--- dags/sql/indexes/raw__transaction__refunds.sql
+++ dags/sql/indexes/raw__transaction__refunds.sql
@@ -1,2 +1,3 @@
 CREATE INDEX IF NOT EXISTS raw__transaction_refunds__transaction_id_idx ON {{ schema }}.raw__transaction__refunds (transaction_id);
 CREATE INDEX IF NOT EXISTS raw__transaction_refunds__transactionitem_id_idx ON {{ schema }}.raw__transaction__refunds (transactionitem_id);
+CREATE INDEX IF NOT EXISTS transaction_refunds_compound_idx ON raw__transaction__refunds (transaction_id, transactionitem_id);
--- dags/sql/indexes/raw__transaction__try_ons.sql
+++ dags/sql/indexes/raw__transaction__try_ons.sql
@@ -1,2 +1,3 @@
 CREATE INDEX IF NOT EXISTS raw__transaction_tryons__transaction_id_idx ON {{ schema }}.raw__transaction__try_ons (transaction_id);
 CREATE INDEX IF NOT EXISTS raw__transaction_tryons__transactionitem_id_idx ON {{ schema }}.raw__transaction__try_ons (transactionitem_id);
+CREATE INDEX IF NOT EXISTS transaction_try_ons_compound_idx ON raw__transaction__try_ons (transaction_id, transactionitem_id);
