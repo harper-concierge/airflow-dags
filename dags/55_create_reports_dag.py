b3a6eacc560c41caf7dd7fad2467b5b991c86e7e
Martin Moss <martyzz1@gmail.com>
2025-01-07 16:14:03 +0000

    [main] Get rid of obsolete report build pool

--- dags/55_create_reports_dag.py
+++ dags/55_create_reports_dag.py
@@ -14,7 +14,7 @@ default_args = {
     "depends_on_past": False,
     "retry_delay": timedelta(minutes=5),
     "retries": 0,
-    "pool": "report_build_pool",
+    "pool": "sql_single_thread_pool",
 }
 
 sql_type = "reports"
