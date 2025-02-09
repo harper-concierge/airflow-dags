6809dbd46f6f5dd70847e49502d2bdfd8ecf2d5e
Martin Moss <martyzz1@gmail.com>
2025-02-09 10:43:41 +0000

    [main] The if Modified isn't helpful here

--- dags/sql/users/tableau.sql
+++ dags/sql/users/tableau.sql
@@ -1,4 +1,3 @@
-{% if is_modified %}
 DO
 $do$
 BEGIN
@@ -9,33 +8,30 @@ BEGIN
     ELSE
         CREATE ROLE {{ tableau_username }} LOGIN PASSWORD '{{ tableau_password }}';
         -- CREATE USER {{ tableau_username }} WITH PASSWORD '{{ tableau_password }}';
+
+        -- Grant connect on the "datalake" database
+        GRANT CONNECT ON DATABASE datalake TO tableau;
+
+        -- Grant usage on the public schema
+        GRANT USAGE ON SCHEMA public TO tableau;
+
+        -- Set default privileges for tables
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO tableau;
+
+        -- Set default privileges for sequences
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO tableau;
+
+        -- Set default privileges for functions
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO tableau;
+
+        -- Grant select on all existing tables in the public schema
+        GRANT SELECT ON ALL TABLES IN SCHEMA public TO tableau;
+
+        -- Grant select on all sequences in the public schema
+        GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO tableau;
+
+        -- Grant execute on all functions in the public schema
+        GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO tableau;
     END IF;
 END
 $do$;
-
--- Grant connect on the "datalake" database
-GRANT CONNECT ON DATABASE datalake TO tableau;
-
--- Grant usage on the public schema
-GRANT USAGE ON SCHEMA public TO tableau;
-
--- Set default privileges for tables
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO tableau;
-
--- Set default privileges for sequences
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON SEQUENCES TO tableau;
-
--- Set default privileges for functions
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT EXECUTE ON FUNCTIONS TO tableau;
-
--- Grant select on all existing tables in the public schema
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO tableau;
-
--- Grant select on all sequences in the public schema
-GRANT SELECT ON ALL SEQUENCES IN SCHEMA public TO tableau;
-
--- Grant execute on all functions in the public schema
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO tableau;
-{% else %}
-SELECT NULL;
-{% endif %}
