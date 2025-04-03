eeb48f699b139ac437b8421b453d592c27d7cb7a
Martin Moss <martyzz1@gmail.com>
2025-04-03 12:02:24 +0100

    [main] Remove deprecated invoice field

--- dags/sql/indexes/raw__stripe_charges.sql
+++ dags/sql/indexes/raw__stripe_charges.sql
@@ -1,2 +1,2 @@
-CREATE INDEX IF NOT EXISTS raw__stripe__charges__invoice_idx ON {{ schema }}.raw__stripe__charges (invoice);
+--CREATE INDEX IF NOT EXISTS raw__stripe__charges__invoice_idx ON {{ schema }}.raw__stripe__charges (invoice);
 CREATE INDEX IF NOT EXISTS raw__stripe__charges__created_idx ON {{ schema }}.raw__stripe__charges (created);
