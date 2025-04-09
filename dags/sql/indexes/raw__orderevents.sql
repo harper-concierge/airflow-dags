277bbef991d50daf9f747b1b2fb07c0a0a403a6e
Martin Moss <martyzz1@gmail.com>
2025-04-09 08:58:49 +0100

    [main] oops

--- dags/sql/indexes/raw__orderevents.sql
+++ dags/sql/indexes/raw__orderevents.sql
@@ -1,2 +1,2 @@
-CREATE INDEX IF NOT EXISTS raw__orders__eventnameid_idx ON {{ schema }}.raw__orders (event_name_id);
-CREATE INDEX IF NOT EXISTS raw__orders__orderid_idx ON {{ schema }}.raw__orders (order_id);
+CREATE INDEX IF NOT EXISTS raw__orders__eventnameid_idx ON {{ schema }}.raw__orderevents (event_name_id);
+CREATE INDEX IF NOT EXISTS raw__orders__orderid_idx ON {{ schema }}.raw__orderevents (order_id);
