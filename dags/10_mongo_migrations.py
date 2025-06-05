e4060c46085b00f35475e3f2da21a8fc63077859
Martin Moss <martyzz1@gmail.com>
2025-06-05 12:16:57 +0100

    [main] if a previous dagrun fails lets block until we are good.

--- dags/10_mongo_migrations.py
+++ dags/10_mongo_migrations.py
@@ -63,8 +63,8 @@ dag = DAG(
 
 wait_for_things_to_exist = ExternalTaskSensor(
     task_id=f"wait_for_things_to_exist_rebuild_{rebuild}",
-    external_dag_id="01_ensure_things_exist",  # The ID of the DAG you're waiting for
-    depends_on_past=False,
+    external_dag_id="10_mongo_migrations_dag",  # The ID of the DAG you're waiting for
+    depends_on_past=True,
     external_task_id=None,  # Set to None to wait for the entire DAG to complete
     allowed_states=["success"],  # You might need to customize this part
     dag=dag,
