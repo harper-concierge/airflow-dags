98f5206caf45ccd45abc67fa795ff31df69e01ab
Martin Moss <martyzz1@gmail.com>
2025-06-08 13:30:30 +0100

    [main] Try to get this doing the right thing

--- dags/10_mongo_migrations.py
+++ dags/10_mongo_migrations.py
@@ -62,9 +62,10 @@ dag = DAG(
 )
 
 wait_for_things_to_exist = ExternalTaskSensor(
-    task_id=f"wait_for_things_to_exist_rebuild_{rebuild}",
+    task_id="wait_for_things_to_exist",
     external_dag_id="10_mongo_migrations_dag",  # The ID of the DAG you're waiting for
     depends_on_past=True,
+    catchup=True,
     external_task_id=None,  # Set to None to wait for the entire DAG to complete
     allowed_states=["success"],  # You might need to customize this part
     dag=dag,
